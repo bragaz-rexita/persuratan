d992afe285024663218f89075b183a6ed9e14bc9
bragaz_rexita <bragazrexi@gmail.com>
2026-07-30 19:32:54 +0700

    repair

--- app/Http/Controllers/RecruitmentController.php
+++ app/Http/Controllers/RecruitmentController.php
@@ -173,7 +173,6 @@ class RecruitmentController extends Controller
         } else {
 			$urutanwerno= array('red','green','blue','yellow','navy','teal','orange','maroon','black','aqua');
 			$groups     = MasterPS::where('kodeps', 'aktif')->orderBy('tanggal', 'DESC')->limit(30)->get();
-            dd($groups);
 			$y      	= 0;
 			$x      	= 0;
 			foreach ($groups as $rowpeng) {
@@ -216,8 +215,8 @@ class RecruitmentController extends Controller
 				$data['pengumumans'][$x]['icon']        =   'fa-bullhorn';
 				$data['pengumumans'][$x]['urutanwerno'] =   'red';
 			}
-			$qrcode 			= base64_encode(QrCode::format('png')->size(100)->generate($lamanapps01));
-			$data['qrcode'] 	= $qrcode;
+			// $qrcode 			= base64_encode(QrCode::format('png')->size(100)->generate($lamanapps01));
+			// $data['qrcode'] 	= $qrcode;
         	return view('rekrutmen.login', $data);
         }
     }
