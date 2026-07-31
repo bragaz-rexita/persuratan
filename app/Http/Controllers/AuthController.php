9c0b729a18e360f11f921a8eb783cd3d0cc83f62
bragaz_rexita <bragazrexi@gmail.com>
2026-07-31 21:28:38 +0700

    repair

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -197,6 +197,10 @@ class AuthController extends Controller
 			$url = 'http://surat-ptdpm.rs-primahusada.id/rsphportal';
 			return Redirect::to($url);
 		}
+        else if ($domain == 'http://surat-rsphm.rs-primahusada.id' OR $domain == 'www.surat-rsphm.rs-primahusada.id') {
+			$url = 'http://surat-rsphm.rs-primahusada.id/rsphportal';
+			return Redirect::to($url);
+		}
         // else if ($domain == 'https://app.rs-primahusada.id/e-office' OR $domain == 'www.app.rs-primahusada.id/e-office') {
 		// 	$url = 'https://app.rs-primahusada.id/e-office/rsphportal';
 		// 	return Redirect::to($url);
