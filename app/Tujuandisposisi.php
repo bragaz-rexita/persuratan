20a42ff51e22fed16bfbef7d05029fd43509b313
bragaz_rexita <bragazrexi@gmail.com>
2026-08-01 07:14:34 +0700

    add file

---
+++ app/Tujuandisposisi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tujuandisposisi extends Model
+{
+    protected $table    =   "tbl_tujuandisposisi";
+    public $timestamps  =   false;
+    protected $guarded  =   [];
+}
