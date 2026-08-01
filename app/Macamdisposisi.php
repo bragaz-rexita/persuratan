9a1a538a7a8e34627121950a6f077d6110157619
bragaz_rexita <bragazrexi@gmail.com>
2026-08-01 07:15:34 +0700

    add file

---
+++ app/Macamdisposisi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Macamdisposisi extends Model
+{
+    protected $table    =   "tbl_macamdisposisi";
+    public $timestamps  =   false;
+    protected $guarded  =   [];
+
+}
