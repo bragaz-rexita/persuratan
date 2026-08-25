14123dd82edf10e2bc3d60bbff60592b5998da52
bragaz_rexita <bragazrexi@gmail.com>
2026-08-25 13:04:16 +0700

    Repair route auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -221,6 +221,10 @@ class AuthController extends Controller
 			$url = 'http://172.100.0.23/rsphportal';
 			return Redirect::to($url);
 		}
+        else if ($domain == '172.100.0.24:80' OR $domain == '172.100.0.24') {
+			$url = 'http://172.100.0.24/rekrutmen';
+			return Redirect::to($url);
+		}
         else if ($domain == 'http://100.69.34.63:80' OR $domain == '100.69.34.63') {
 			$url = 'http://100.69.34.63/rsphportal';
 			return Redirect::to($url);
